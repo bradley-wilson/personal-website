aee29b83dfc8279c1d65508d862320bfb2b653fc
Sergio Ribero <sergio.ribero@icloud.com>
2019-01-10 10:52:41 -0500

    Added Publications page

---
+++ pages/publications/index.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="container">
+    <h1 class="heading heading--primary">Publications</h1>     
+
+    <h2 class="heading heading--secondary">Journals</h2>
+    <Publication :publications="journals"/>
+
+    <h2 class="heading heading--secondary">Book Chapters</h2>
+    <Publication :publications="bookChapters"/>
+
+    <h2 class="heading heading--secondary">Conferences</h2>
+    <Publication :publications="conferences"/>
+
+    <h2 class="heading heading--secondary">Other</h2>
+    <Publication :publications="other"/>
+  </div>
+</template>
+
+<script>
+import Publication from '@/components/Publication'
+
+export default {
+  async asyncData({ $axios }) {
+    const token = '?token=4458f0a2d0d2793a50fe20d0e9c519'
+    let journalsRes = await $axios.$get(
+      '/cockpit/article' + token + '&filter[type]=Journal&sort[year]=-1'
+    )
+    let bookChaptersRes = await $axios.$get(
+      '/cockpit/article' + token + '&filter[type]=Book+chapter&sort[year]=-1'
+    )
+    let conferencesRes = await $axios.$get(
+      '/cockpit/article' + token + '&filter[type]=Conference&sort[year]=-1'
+    )
+    let otherRes = await $axios.$get(
+      '/cockpit/article' + token + '&filter[type]=Other&sort[year]=-1'
+    )
+    return {
+      journals: journalsRes.entries,
+      bookChapters: bookChaptersRes.entries,
+      conferences: conferencesRes.entries,
+      other: otherRes.entries
+    }
+  },
+  components: {
+    Publication
+  }
+}
+</script>
