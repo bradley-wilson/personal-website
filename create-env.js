36b5592400b3676fa7bceaa3c1db256116981ef0
Sergio Ribero <sergio.ribero@icloud.com>
2019-08-15 12:51:51 -0500

    Added create-env.js

---
+++ create-env.js
@@ -0,0 +1,13 @@
+const fs = require('fs')
+fs.writeFileSync('./.env', `
+BASE_URL=${process.env.BASE_URL}\n
+EVENTS_URL=${process.env.EVENTS_URL}\n
+QUOTES_URL=${process.env.QUOTES_URL}\n
+PUBLICATIONS_URL=${process.env.PUBLICATIONS_URL}\n
+CONTACTS_URL=${process.env.CONTACTS_URL}\n
+STATS_URL=${process.env.STATS_URL}\n
+AWARDS_URL=${process.env.AWARDS_URL}\n
+BIOS_URL=${process.env.BIOS_URL}\n
+POSTS_URL=${process.env.POSTS_URL}\n
+URL=${process.env.URL}
+`)
