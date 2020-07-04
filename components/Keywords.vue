34922fb3e369a99d17d3412afba6f23ae39f8af6
Sergio Ribero <sergio.ribero@icloud.com>
2020-07-04 15:41:55 -0500

    Changed keywords styling

--- components/Keywords.vue
+++ components/Keywords.vue
@@ -26,49 +26,20 @@ export default {
       }
     }
   },
-  data() {
-    return {
-      slideCounter: 1
-    }
-  },
   mounted: function() {
-    this.arrangeSlides()
-    this.carouselMoveForward()
   },
   methods: {
-    carouselMoveForward: function() {
-      this.$refs.carousel.style.transform = 'translateX(' + this.$refs.searches.length + '%)'
-      setInterval(this.$refs.carousel.style.transform = 'translateX(0%)', 15000)
-      carouselMoveForward()
-    },
-    arrangeSlides: function() {
-      for (let i = 0; i < this.$refs.searches.length; i++) {
-        let offset = 100 * i
-        this.$refs.searches[i].style.transform = 'translateX(' + offset + '%)'
-      }
-    }
   }
 }
 </script>
 
 <style scoped lang="scss">
 .carousel {
-  position: relative;
-  height: 15rem;
 
   &__content {
-    // height: 100%;
-    overflow: hidden;
-    position: relative;
   }
 
   &__track {
-    position: absolute;
-    top: 0;
-    left: 0;
-    bottom: 0;
-    right: 0;
-    transition: transform 15s ease 0s;
   }
 }
 </style>
