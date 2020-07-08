05f8c70f7a4a4e3983d877940188e3c9ce70eee8
Sergio Ribero <sergio.ribero@icloud.com>
2020-07-08 10:28:53 -0500

    Changes to graphs

--- components/Stats.vue
+++ components/Stats.vue
@@ -19,13 +19,6 @@ export default {
         tooltips: {
           mode: 'point'
         },
-        scales: {
-          yAxes: [{
-            id: 'Reads',
-            type: 'linear',
-            position: 'left'
-          }]
-        },
         layout: {
           padding: {
             top: 30,
