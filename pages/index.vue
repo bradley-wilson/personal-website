06003bf80f6c44540e36065b5d6b605581087824
Sergio Ribero <sergio.ribero@icloud.com>
2020-07-15 10:34:27 -0500

    Changes to update stats

--- pages/index.vue
+++ pages/index.vue
@@ -394,6 +394,31 @@ export default {
     let impactStoryRes = await $axios.$get('https://profiles.impactstory.org/api/person/0000-0002-3730-6295')
     let statsRes = await $axios.$get(`${env.statsUrl}&sort[week]=1`)
 
+    let weekReads = scrappedRes.Researchgate['C4_Weekly change']
+    let weekInterest = scrappedRes.Researchgate['C1_Weekly change']
+
+    weekInterest.split('+').pop()
+    weekReads.split('+').pop()
+
+    let statsData = {
+      week: scrappedRes.Researchgate.weekdate,
+      reads: weekReads.split('+').pop(),
+      interest: weekInterest.split('+').pop()
+    }
+
+    await $axios.$post(
+      'https://www.bradwilsonphd.com/cockpit/api/collections/save/stats?token=4458f0a2d0d2793a50fe20d0e9c519',
+      JSON.stringify({
+        statsData
+      }),
+      {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }
+    )
+
+
     let citationYears = Object.getOwnPropertyNames(scrappedRes.GoogleScholar.Total)
     let citationValues = []
     for (let key in scrappedRes.GoogleScholar.Total) {
@@ -491,7 +516,7 @@ export default {
   },
   mounted: function() {
     setInterval(this.statsMoveForward, 10000)
-    this.updateStats()
+    // this.updateStats()
   },
   methods: {
     toTarget: function(target) {
@@ -530,16 +555,28 @@ export default {
         interest: weekInterest.split('+').pop()
       }
 
-      fetch('https://www.bradwilsonphd.com/cockpit/api/collections/save/stats?token=4458f0a2d0d2793a50fe20d0e9c519', {
-        method: 'post',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-          data: statsData
-        })
-      })
-      // .then(res=>res.json())
-      // .then(entry => console.log(entry));
-    }
+      this.$axios.$post(
+        'https://www.bradwilsonphd.com/cockpit/api/collections/save/stats?token=4458f0a2d0d2793a50fe20d0e9c519',
+        JSON.stringify({
+          statsData
+        }),
+        {
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        }
+      )
+
+    //   fetch('https://www.bradwilsonphd.com/cockpit/api/collections/save/stats?token=4458f0a2d0d2793a50fe20d0e9c519', {
+    //     method: 'post',
+    //     headers: { 'Content-Type': 'application/json' },
+    //     body: JSON.stringify({
+    //       data: statsData
+    //     })
+    //   })
+    //   // .then(res=>res.json())
+    //   // .then(entry => console.log(entry));
+    // }
   },
   layout: 'landing-page'
 }
