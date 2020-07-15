0c4bc695c12d46fcc293c0abdfbcb0a1d171f5a5
Sergio Ribero <sergio.ribero@icloud.com>
2020-07-15 08:49:38 -0500

    Changes to update stats function

--- pages/index.vue
+++ pages/index.vue
@@ -530,7 +530,7 @@ export default {
         [this.scrappedData.Researchgate.weekdate]: weekInterest
       }
 
-      if (this.readsData.datasets.data[this.readsData.datasets.data.length - 1].week != this.statsData.week) {
+      if (this.readsData.datasets[0].data[this.readsData.datasets[0].data.length - 1].week != this.statsData.week) {
       this.$axios.$post(
           env.weekUrl,
           JSON.stringify({
