c72d648475fbbb2b3a491d3b0f3faeb8257a2e01
chechoribero <andres.riberobautista@gmail.com>
2020-09-06 20:23:34 -0500

    Created cookie banner component

---
+++ components/CookiesBanner.vue
@@ -0,0 +1,32 @@
+<template>
+    <div
+    class="cookies-banner"
+    style="display: none">
+        <p>By using this website you agree to our <a href="#">cookie policy</a></p>
+        <button class="cookies-button">Accept</button>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.cookies {
+    &--banner {
+    position: fixed;
+    bottom: 40px;
+    left: 10%;
+    right: 10%;
+    width: 80%;
+    padding: 5px 14px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    background-color: $light-blue;
+    border-radius: 5px;
+    }
+    
+    &--button {
+        padding: .5rem 1rem;
+        background-color: $color-secondary;
+    }
+}
+
+</style>
