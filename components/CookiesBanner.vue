15d8848684481f21ee2d267f480f83753cee80c6
chechoribero <andres.riberobautista@gmail.com>
2020-09-06 21:57:43 -0500

    Set up cookies banner component

--- components/CookiesBanner.vue
+++ components/CookiesBanner.vue
@@ -1,31 +1,49 @@
 <template>
     <div
-    class="cookies-banner"
-    style="display: none">
+    class="cookies--banner"
+    id="cookies--banner">
         <p>By using this website you agree to our <a href="#">cookie policy</a></p>
-        <button class="cookies-button">Accept</button>
+        <button
+        @click="document.getElementById('cookies--banner').style.display = none"
+        class="cookies--button">Accept</button>
     </div>
 </template>
+
+<script>
+export default {
+    mounted: function() {
+        let banner = document.getElementById('cookies--banner')
+
+        if (localStorage.getItem('cookieSeen') != 'shown') {
+            banner.style.display = block
+            localStorage.setItem('cookieSeen') = 'shown'
+        }
+
+    }
+}
+</script>
 
 <style lang="scss" scoped>
 .cookies {
     &--banner {
-    position: fixed;
-    bottom: 40px;
-    left: 10%;
-    right: 10%;
-    width: 80%;
-    padding: 5px 14px;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    background-color: $light-blue;
-    border-radius: 5px;
+        position: fixed;
+        bottom: 40px;
+        left: 10%;
+        right: 10%;
+        width: 80%;
+        padding: 5px 14px;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        background-color: $light-blue;
+        border-radius: 5px;
+        display: none;
     }
     
     &--button {
         padding: .5rem 1rem;
         background-color: $color-secondary;
+        border-radius: 5px;
     }
 }
 
